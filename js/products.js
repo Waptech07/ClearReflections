bebc0ce6a4628fe2f6f42dae49fe01dc3c6b5a14
Yakub Abdulfatai <abdulfataiyakub07@gmail.com>
2023-02-11 18:51:17 +0100

    Updated Product Page

--- js/products.js
+++ js/products.js
@@ -1,31 +1,220 @@
 let products = [
   {
     id: 0,
-    image: "../images/pngimg.com - mirror_PNG17358.png",
-    name: "Classic generic mirror 1",
-    price: "$20.00",
+    image: "../images/Wooden Framed/rustic-wall-mirrors.jpg",
+    name: "El Paso Vanity Accent Mirror",
+    price: "$185.00",
+    category: "Drawing / Living Room Mirrors",
+    material: "Wooden Framed",
+    description: "We take pine and create this wonderful mirror.a This entire mirror measures approximately 23 x 35 inches. The mirror part itself, measures about 16 x 28 in. This rustic wood mirror is perfect for any cowboy, western, lodge, cabin decor. You can hang either way. It comes with two saw tooth hangers."
+  },
+  {
+    id: 1,
+    image: "../images/Acrylic Bordered/Savoy-Acrylic-Gold-Beveled-Mirror.jpeg",
+    name: "Savoy Acrylic Gold Beveled Mirror",
+    price: "$220.00",
     category: "Bathroom Mirrors",
-    material: "Wooden Frame",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quam optio, aliquam quia necessitatibus, explicabo illo veritatis eveniet voluptas laborum quaerat unde, corrupti earum assumenda corporis quae iusto sint alias."
+    material: "Acrylic Bordered",
+    description: "This entire mirror measures approximately (Height) 64cm x 46cm(length)."
   },
   {
     id: 2,
-    image: "../images/pngimg.com - mirror_PNG17358.png",
-    name: "Classic generic mirror 2",
+    image: "../images/Metal Bordered/Metal Frame Dressing Mirror.png",
+    name: "Metal Oval Dressing Mirror",
     price: "$20.00",
-    category: "Bathroom Mirrors",
-    material: "Wooden Frame",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quam optio, aliquam quia necessitatibus, explicabo illo veritatis eveniet voluptas laborum quaerat unde, corrupti earum assumenda corporis quae iusto sint alias."
+    category: "Dressing Mirrors",
+    material: "Metal Bordered",
+    description: "A beautiful  metal mirror. This entire mirror measures 80cm diameter weight 10kgs "
   },
   {
     id: 3,
-    image: "../images/pngimg.com - mirror_PNG17358.png",
-    name: "Classic generic mirror 3",
-    price: "$20.00",
+    image: "../images/Decorative Mirrors/Wall Mirror Bedroom Mother of Pearl Inlay Oval Frame - Etsy.webp",
+    name: "Mother of Pearl Oval Frame Mirror",
+    price: "$300.00",
+    category: "Bedroom Mirrors",
+    material: "Decorative Mirrors",
+    description: "The pipers round measures 47.2Length x 1.0Weight x 47.2Height."
+  },
+  {
+    id: 4,
+    image: "../images/Led Mirrors/illume-rectangle-led-backlit-dressing-mirror.png",
+    name: "Illume Rectangle Led Backlit",
+    price: "$285.00",
+    category: "Drawing / Living Room Mirrors",
+    material: "Led Mirrors",
+    description: "This durable led mirror measures 165cm Height 75cm Width 3.5cm depth."
+  },
+  {
+    id: 5,
+    image: "../images/Bluetooth Mirrors/Corona_LED_Bluetooth_Mirror.jpg",
+    name: "Corona LED Bluetooth Mirror",
+    price: "$480.00",
     category: "Bathroom Mirrors",
-    material: "Wooden Frame",
+    material: "Bluetooth Mirrors",
+    description: "Bluetooth mirror measures 165cm x 65cm and the bluetooth frame is 7'(17cm)with a profile thickness of 10cm."
+  },
+  {
+    id: 6,
+    image: "../images/Wooden Framed/wooden-framed-dressing-mirror.png",
+    name: "Wooden Framed Dressing Mirror",
+    price: "$260.00",
+    category: "Dressing Mirrors",
+    material: "Wooden Framed",
+    description: "The internal measures is 89.5Weight x 9.5Diameter x 175.5cm Height ."
+  },
+  {
+    id: 7,
+    image: "../images/Acrylic Bordered/salisbury-glass-mirror.jpg",
+    name: "Salisbury Glass Mirror",
+    price: "$80.00",
+    category: "Bedroom Mirrors",
+    material: "Acrylic Bordered",
+    description: "Acrylic Bordered measures 23Height x 28Length ."
+  },
+  {
+    id: 8,
+    image: "../images/Metal Bordered/mughal-bone-mirror.jpg",
+    name: "Mughal Bone Metal Mirror",
+    price: "$180.00",
+    category: "Drawing / Living Room Mirrors",
+    material: "Metal Bordered",
+    description: "Measures 16 x20 iches"
+  },
+  {
+    id: 9,
+    image: "../images/Decorative Mirrors/ulric-decorative-vanity-mirror.webp",
+    name: "Ulric Decorative Vanity Mirror",
+    price: "$240.00",
+    category: "Bathroom Mirrors",
+    material: "Decorative Mirrors",
+    description:"This measures 60cm height x 140cm Weight."
+  },
+  {
+    id: 10,
+    image: "../images/Led Mirrors/led-dimmable-electric-rectangle.jpg",
+    name: "Led Dimmable Electric Mirror",
+    price: "$250.00",
+    category: "Dressing Mirrors",
+    material: "Led Mirrors",
+    description: "Measure approximately 30cm length 50cm Weight."
+  },
+  {
+    id: 11,
+    image: "../images/Bluetooth Mirrors/Dionysus-bluetooth-mirror.png",
+    name: "Dionysus Bluetooth Mirror",
+    price: "$450.00",
+    category: "Bedroom Mirrors",
+    material: "Bluetooth Mirrors",
+    description:"This bedroom suit all rooms (30cm Weight x30cm height)"
+  },
+  {
+    id: 12,
+    image: "../images/Acrylic Bordered/Ghost-Acrylic-Round-Mirror.jpg",
+    name: "Gold Acrylic Round Mirror",
+    price: "$240.00",
+    category: "Drawing / Living Room Mirrors",
+    material: "Acrylic Bordered",
+    description: "."
+  },
+  {
+    id: 13,
+    image: "../images/Metal Bordered/metal-framed-bathroom-mirror.jpg",
+    name: "Metal Framed Bathroom Mirror",
+    price: "$130.00",
+    category: "Bathroom Mirrors",
+    material: "Metal Bordered",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quam optio, aliquam quia necessitatibus, explicabo illo veritatis eveniet voluptas laborum quaerat unde, corrupti earum assumenda corporis quae iusto sint alias."
   },
+  {
+    id: 14,
+    image: "../images/Acrylic Bordered/Structure-Rectangle-Brass-Acrylic-Wall-Mirror.jpeg",
+    name: "Stucture Rectangle Brass Wall Mirror",
+    price: "$210.00",
+    category: "Dressing Mirrors",
+    material: "Acrylic Bordered",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quam optio, aliquam quia necessitatibus, explicabo illo veritatis eveniet voluptas laborum quaerat unde, corrupti earum assumenda corporis quae iusto sint alias."
+  },
+  {
+    id: 15,
+    image: "../images/Metal Bordered/metal-bordered-2.webp",
+    name: "Metal Bordered Bedroom Mirror",
+    price: "$180.00",
+    category: "Bedroom Mirrors",
+    material: "Metal Bordered",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quam optio, aliquam quia necessitatibus, explicabo illo veritatis eveniet voluptas laborum quaerat unde, corrupti earum assumenda corporis quae iusto sint alias."
+  },
+  {
+    id: 16,
+    image: "../images/Bluetooth Mirrors/miseno-led-bluetooth-living-room-Mirror.png",
+    name: "Miseno Bluetooth Living Room Mirror",
+    price: "$500.00",
+    category: "Drawing / Living Room Mirrors",
+    material: "Bluetooth Mirrors",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quam optio, aliquam quia necessitatibus, explicabo illo veritatis eveniet voluptas laborum quaerat unde, corrupti earum assumenda corporis quae iusto sint alias."
+  },
+  {
+    id: 17,
+    image: "../images/Wooden Framed/wooden-framed-bathroom-mirror.png",
+    name: "Wooden Framed Bathroom Mirror",
+    price: "$20.00",
+    category: "Bathroom Mirrors",
+    material: "Wooden Framed",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quam optio, aliquam quia necessitatibus, explicabo illo veritatis eveniet voluptas laborum quaerat unde, corrupti earum assumenda corporis quae iusto sint alias."
+  },
+  {
+    id: 18,
+    image: "../images/Bluetooth Mirrors/bluetooth-led-dressing-mirror.png",
+    name: "Bluetooth Led Dressing Mirror",
+    price: "$250.00",
+    category: "Dressing Mirrors",
+    material: "Bluetooth Mirrors",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quam optio, aliquam quia necessitatibus, explicabo illo veritatis eveniet voluptas laborum quaerat unde, corrupti earum assumenda corporis quae iusto sint alias."
+  },
+  {
+    id: 19,
+    image: "../images/Wooden Framed/Contemporary Natural Solid Oak Wall Mirror.jpg",
+    name: "Contemporary Natural Solid Oak Wall Mirror",
+    price: "$40.00",
+    category: "Bedroom Mirrors",
+    material: "Wooden Framed",
+    description: "This Contemporary mirror has a natural solid oak hardwood frame and has a modern feel with a groove design at the frame corners. The fixings on the rear allow it to be hung either in portrait or landscape. This mirror would look fantastic with any of our solid oak furniture, giving that final touch to any room."
+  },
+  {
+    id: 20,
+    image: "../images/Decorative Mirrors/contemporary-wall-mirrors.jpg",
+    name: "Contemporary Wall Living Room Mirror",
+    price: "$120.00",
+    category: "Drawing / Living Room Mirrors",
+    material: "Decorative Mirrors",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quam optio, aliquam quia necessitatibus, explicabo illo veritatis eveniet voluptas laborum quaerat unde, corrupti earum assumenda corporis quae iusto sint alias."
+  },
+  {
+    id: 21,
+    image: "../images/Led Mirrors/Vanity-art-led-mirror.png",
+    name: "Vanity Art Led Mirror",
+    price: "$270.00",
+    category: "Bathroom Mirrors",
+    material: "Led Mirrors",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quam optio, aliquam quia necessitatibus, explicabo illo veritatis eveniet voluptas laborum quaerat unde, corrupti earum assumenda corporis quae iusto sint alias."
+  },
+  {
+    id: 22,
+    image: "../images/Decorative Mirrors/decorative-wall-mirror.jpg",
+    name: "Decorative Wall Dressing Mirror",
+    price: "$210.00",
+    category: "Dressing Mirrors",
+    material: "Decorative Mirrors",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quam optio, aliquam quia necessitatibus, explicabo illo veritatis eveniet voluptas laborum quaerat unde, corrupti earum assumenda corporis quae iusto sint alias."
+  },
+  {
+    id: 23,
+    image: "../images/Led Mirrors/portable-bedroom-led-mirror.jpg",
+    name: "Portable Led Bedroom Mirror",
+    price: "$320.00",
+    category: "Bedroom Mirrors",
+    material: "Led Mirrors",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quam optio, aliquam quia necessitatibus, explicabo illo veritatis eveniet voluptas laborum quaerat unde, corrupti earum assumenda corporis quae iusto sint alias."
+  }
 ];
 
 
@@ -56,15 +245,15 @@ function parseContents(category, material) {
 
 function setProducts(product) {
   const html = ` 
-<div class="product-card">
-        <a href="#" class="details" id="${product.id}">
-          <i class="fas fa-info"></i>                                                         
-        </a>
-<img src="${product.image}" alt="Product 1">
-<h3>${product.name}</h3>
-<p>${product.price}</p>
-        <button class="add-to-cart">Add to Cart</button>
-      </div>
+    <div class="product-card">
+      <a href="#" class="details" id="${product.id}">
+        <i class="fas fa-info"></i>                                                         
+      </a>
+      <img src="${product.image}" alt="Product 1">
+      <h3>${product.name}</h3>
+      <p>${product.price}</p>
+      <button class="add-to-cart">Add to Cart</button>
+    </div>
     `;
   $("#products-slot").append(html)
 }
@@ -74,14 +263,17 @@ function setDialog(product) {
   const html = `
     <div class="card">
       <section class="sticky">
-<p class="card-title">${product.name}</p>
-<p><span>${product.category} Mirrors</span> <span class="material-chip">${product.material}</span></p>
+        <a href="javascript:void(0);" class="close-dialog">
+          <i class="fas fa-times"></i>
+        </a>
+        <p class="card-title">${product.name}</p>
+        <p><span>${product.category}</span> <span class="material-chip">${product.material}</span></p>
       </section>
       <section class="grid" style="margin-top: 25px;">
-<img src="${product.image}" alt="Mirror image" class="center image">
-<p>${product.description}</p>
+        <img src="${product.image}" alt="Mirror image" class="center image">
+        <p>${product.description}</p>
       </section>
-<a href="file.text" class="download-btn" id="${product.id}">Download</a>
+      <a href="file.text" class="download-btn" id="${product.id}">Download</a>
     </div>
 `;
   $("#dialog").append(html);
@@ -93,11 +285,11 @@ const addDownloadHandler = () => {
     const item = products.filter((e) => `${e.id}` === this.id)
     const detail = item[0];
     const fileContent = `
-Title: ${detail.name},
-category: ${detail.category},
-Material: ${detail.material},
-Description: ${detail.description}
-`;
+    Title: ${detail.name},
+    category: ${detail.category},
+    Material: ${detail.material},
+    Description: ${detail.description}
+  `;
     const link = document.createElement("a");
     const file = new Blob([fileContent], { type: 'text/plain;charset=utf-8' })
     link.href = URL.createObjectURL(file);
@@ -129,6 +321,10 @@ $(document).ready(function() {
     setDialog(item[0])
     $("#dialog").toggleClass("hidden");
     addDownloadHandler();
+  $(".close-dialog").click(function(event) {
+    $("#dialog").addClass("hidden");
+  });
+  
   })
   $("#dialog").click(function(event) {
     if (event.target !== this) {
